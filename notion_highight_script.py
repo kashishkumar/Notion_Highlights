aff171f0373dbaf387cbf07cbad4d13023ba93c8
Kashish Kumar <kashish31894@gmail.com>
2021-11-09 03:42:53 +0530

    removed token

--- notion_highight_script.py
+++ notion_highight_script.py
@@ -1,17 +1,17 @@
 import notion
 from notion.client import NotionClient
 
-    """
-    Login into notion and access to file
-    Store selected lines in a list 
-    Print the list to a csv file
+"""
+Login into notion and access to file
+Store selected lines in a list 
+Print the list to a csv file
 
-    Returns:
-        csv file with all the selected lines
-    """
+Returns:
+    csv file with all the selected lines
+"""
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
-client = NotionClient(token_v2="69250320efcdea81c29e141642390014d952a69875ce7d564953f84d98d7b015c285f73447500983b1aa3e175b6ece197e799ad8c0f7b5b50b5359199a725dd42f3c8ee7c2a2b4bbbd0241aaae93")
+client = NotionClient(token_v2="")
 notion_page_url = "https://www.notion.so/kashishkumar/Python-test-176a5b0ba7974127916ee12311545dd9"
 notion_page2_url = "https://www.notion.so/kashishkumar/Thoughts-e4ae234d7bf9463c885b549682354fb9"
 def get_highlights(notion_page_url):
